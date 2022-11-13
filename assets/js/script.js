94207344a6512db1e688050a93fe7080a8ac50d3
Cristian <duranferreira.cristian@gmail.com>
2022-11-13 00:32:57 -0300

    seccion 2 editada para ocultar en pantallas md y sm, termino seccion contacto y modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
-$(function () {                                // se utilizó en los iconos de la sección_2 ¿Quiénes somos?
-    $('[data-toggle="tooltip"]').tooltip()
+$(function () {                                         //función para los iconos de la sección_2 ¿Quiénes somos?
+  $('[data-toggle="tooltip"]').tooltip()
+});
+
+$(function (){                                          //función para el modal
+  $("#form-suscripcion").on("submit", function (e) {
+    e.preventDefault();
+    var nombre = $("#nombre", this).val();
+    var apellido = $("#apellido", this).val();
+    var email = $("#correo", this).val();
+
+    alert(`Gracias ${nombre} ${apellido} por preferirnos, te contactaremos al ${email}.`)
   })
+});
